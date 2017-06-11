0f302c1b72a3e44637b5c2da6dfee26f0f78edba
Xiangmin Jiao <xmjiao@gmail.com>
2017-06-10 22:55:09 -0400

    Set default resize mode to downscale

--- app/ui.js
+++ app/ui.js
@@ -172,7 +172,7 @@ var UI = {
         UI.initSetting('encrypt', (window.location.protocol === "https:"));
         UI.initSetting('cursor', !isTouchDevice);
         UI.initSetting('clip', false);
-        UI.initSetting('resize', 'off');
+        UI.initSetting('resize', 'downscale');
         UI.initSetting('shared', true);
         UI.initSetting('view_only', false);
         UI.initSetting('path', 'websockify');
